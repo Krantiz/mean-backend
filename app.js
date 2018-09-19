d294b0cf76f545e24d0992aee5e3ab70b2388831
Krantikumar <krantikumar@greatplaceitservices.com>
2018-09-19 15:52:23 +0530

    Setting up basic express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+	console.log('First Middleware');
+	next();
+});
+
+app.use((req, res) => {
+	res.send('Hello expresssssss');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-	res.end('This is my first response');
-});
-
-server.listen(process.env.PORT || 3000);
+app.set('port');
+const server = http.createServer(app);
+server.listen(port);
