383bfca6a0b30a8ce7f871742a2621c944551903
Krantikumar <krantikumar@greatplaceitservices.com>
2018-09-19 16:55:54 +0530

    Resolving CORS error

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-	console.log('First Middleware');
-	next();
+  	res.setHeader("Access-Control-Allow-Origin", "*");
+  	res.setHeader(
+    	"Access-Control-Allow-Headers",
+    		"Origin, X-Requested-With, Content-Type, Accept"
+  	);
+  	res.setHeader(
+    	"Access-Control-Allow-Methods",
+    	"GET, POST, PATCH, DELETE, OPTIONS"
+  	);
+  	next();
 });
 
-app.use((req, res) => {
-	res.send('Hello expresssssss');
+
+app.use('/api/posts',(req, res, next) => {
+	const logs = [
+		{id: 'dsasdasds', un: 'aaa', status: 'Present', Remark: 'asdasdasdasda'},
+		{id: 'wqeradfdf', un: 'bbb', status: 'Absent', Remark: 'asdasdasdasda'},
+		{id: 'vbvzcxzsd', un: 'ccc', status: 'Present', Remark: 'asdasdasdasda'}
+	];
+	res.status(200).json({
+		message: 'fecthed',
+		logs: logs
+	});
 });
 
 module.exports = app;
